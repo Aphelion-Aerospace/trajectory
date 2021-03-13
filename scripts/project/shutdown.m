e9e069d16015331cce6058fa1c9fb96e6327d8b9
Matt Mader <maderm@purdue.edu>
2021-03-13 15:34:51 -0500

    Initial check in

---
+++ scripts/project/shutdown.m
@@ -0,0 +1,8 @@
+%% shutdown.m
+%SHUTDOWN run when the project is closed
+
+% Clear up the workspace
+clearvars,clc,close all
+
+% Visual feedback
+disp("Successful project shutdown!")
---
+++ scripts/project/startup.m
@@ -0,0 +1,8 @@
+%% startup.m
+%STARTUP runs when the project is loaded
+
+% Clear up the workspace
+clearvars,clc,close all
+
+% Visual feedback
+disp("Successful project startup!")
